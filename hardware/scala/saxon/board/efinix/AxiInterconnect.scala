1d33d1e2d62564048fd6c7f2d107499ea43135cb
Dolu1990 <charles.papon.90@gmail.com>
2021-03-26 08:28:53 +0100

    efx sync

--- hardware/scala/saxon/board/efinix/AxiInterconnect.scala
+++ hardware/scala/saxon/board/efinix/AxiInterconnect.scala
@@ -63,3 +63,134 @@ object Axi4_2x32_1x256 {
   }
 }
 
+case class Axi4_2x256_1x256() extends Component{
+  val inputConfig = Axi4Config(
+    addressWidth = 32,
+    dataWidth = 256,
+    idWidth = 4
+  )
+
+  val outputConfig = Axi4Config(
+    addressWidth = 32,
+    dataWidth = 256,
+    idWidth = 5
+  )
+  val io = new Bundle {
+    val inputs = Vec(slave(Axi4(inputConfig)), 2)
+    val output = master(Axi4(outputConfig))
+  }
+
+
+  val arbiter = new Area{
+    val readOnly = Axi4ReadOnlyArbiter(outputConfig, inputsCount = 2)
+    val writeOnly = Axi4WriteOnlyArbiter(outputConfig, inputsCount = 2, routeBufferSize = 4)
+
+    for(i <- 0 to 1){
+      val read = io.inputs(i).toReadOnly()
+      val write = io.inputs(i).toWriteOnly()
+      readOnly.io.inputs(i).ar <-/< read.ar
+      readOnly.io.inputs(i).r >/-> read.r
+      writeOnly.io.inputs(i).aw <-/< write.aw
+      writeOnly.io.inputs(i).w <-/< write.w
+      writeOnly.io.inputs(i).b >/-> write.b
+    }
+
+    io.output.ar <-/< readOnly.io.output.ar
+    io.output.r >/-> readOnly.io.output.r
+    io.output.aw <-/< writeOnly.io.output.aw
+    io.output.w <-/< writeOnly.io.output.w
+    io.output.b >/-> writeOnly.io.output.b
+  }
+}
+
+object Axi4_2x256_1x256 {
+  def main(args: Array[String]): Unit = {
+    SpinalVerilog(Axi4_2x256_1x256())
+  }
+}
+
+case class Axi4_2x_1x_shared(width : Int) extends Component{
+  setDefinitionName(s"Axi4_2x${width}_1x${width}_shared")
+  val inputConfig = Axi4Config(
+    addressWidth = 32,
+    dataWidth = width,
+    idWidth = 4
+  )
+
+  val outputConfig = Axi4Config(
+    addressWidth = 32,
+    dataWidth = width,
+    idWidth = 5
+  )
+
+  val io = new Bundle {
+    val inputs = Vec(slave(Axi4(inputConfig)), 2)
+    val output = master(Axi4Shared(outputConfig))
+    val output_w_payload_id = out(UInt(outputConfig.idWidth bits))
+  }
+
+
+  val arbiter = new Area{
+    val cfg = inputConfig.copy(idWidth = 5)
+    val readOnly = Axi4ReadOnlyArbiter(cfg, inputsCount = 2)
+    val writeOnly = Axi4WriteOnlyArbiter(cfg, inputsCount = 2, routeBufferSize = 4)
+
+    for(i <- 0 to 1){
+      val read = io.inputs(i).toReadOnly()
+      val write = io.inputs(i).toWriteOnly()
+      readOnly.io.inputs(i).ar <-/< read.ar
+      readOnly.io.inputs(i).r >/-> read.r
+      writeOnly.io.inputs(i).aw <-/< write.aw
+      writeOnly.io.inputs(i).w <-/< write.w
+      writeOnly.io.inputs(i).b >/-> write.b
+    }
+  }
+
+
+  val axiNotShared = Axi4(arbiter.cfg)
+  axiNotShared.ar <-/< arbiter.readOnly.io.output.ar
+  axiNotShared.r >/-> arbiter.readOnly.io.output.r
+  axiNotShared.aw <-/< arbiter.writeOnly.io.output.aw
+  axiNotShared.w <-/< arbiter.writeOnly.io.output.w
+  axiNotShared.b >/-> arbiter.writeOnly.io.output.b
+
+  val axiShared = axiNotShared.toShared()
+  val toAxi3Shared = new Area{
+    val (arwFork, wFork) = StreamFork2(axiShared.arw)
+    val wQueue = wFork.translateWith(wFork.id).takeWhen(wFork.write).queueLowLatency(4).pipelined(m2s = true, s2m = true)
+
+    io.output.arw << arwFork
+    io.output.w << axiShared.w.haltWhen(!wQueue.valid)
+    io.output_w_payload_id := wQueue.payload
+
+    wQueue.ready := axiShared.w.fire && axiShared.w.last
+
+    axiShared.r << io.output.r
+    axiShared.b << io.output.b
+  }
+//  val toAxi3Shared = new Area{
+//    val (arwFork, rFork, bFork) = StreamFork3(axiShared.arw)
+//    val rQueue = rFork.translateWith(rFork.id).takeWhen(!rFork.write).pipelined(m2s = true, s2m = true).queue(32).pipelined(m2s = true, s2m = true)
+//    val bQueue = bFork.translateWith(bFork.id).takeWhen( bFork.write).pipelined(m2s = true, s2m = true).queue(32).pipelined(m2s = true, s2m = true)
+//
+//    io.output.arw << arwFork
+//    io.output.w << axiShared.w
+//    io.output.arw.id.removeAssignments() := 0
+//
+//    rQueue.ready := axiShared.r.fire && axiShared.r.last
+//    bQueue.ready := axiShared.b.fire
+//
+//    axiShared.r << io.output.r.haltWhen(!rQueue.valid)
+//    axiShared.b << io.output.b.haltWhen(!bQueue.valid)
+//
+//    axiShared.r.id.removeAssignments() := rQueue.payload
+//    axiShared.b.id.removeAssignments() := bQueue.payload
+//  }
+}
+
+object Axi4_2x_1x_shared {
+  def main(args: Array[String]): Unit = {
+    SpinalVerilog(Axi4_2x_1x_shared(256))
+    SpinalVerilog(Axi4_2x_1x_shared(128))
+  }
+}
